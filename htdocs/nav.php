13cfef6f6a870224562e289d7c2c20508a5dabb0
gdh <gdh@gmail.com>
2025-06-25 03:11:15 +0000

    THIS WILL 100% BREAK THE SITE!!!

--- htdocs/nav.php
+++ htdocs/nav.php
@@ -2,6 +2,7 @@
   <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "changelog"){echo 'active-nav-item';}?>" href="https://gdhplatformers.hpsk.me/index.php">Platformers</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "legacy"){echo 'active-nav-item';}?>" href="legacy.php">Legacy List</a></li>
+    <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "legacy"){echo 'active-nav-item';}?>" href="InsaneDemonList.php">Insane Demon List</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "extended"){echo 'active-nav-item';}?>" href="extended.php">Extended List</a></li>
     <li class="redir"><a class="nav-buttons <?php if($_SESSION['active-nav'] == "index"){echo 'active-nav-item';}?>" href="index.php">Main List</a></li>
     <li class="titlebox"><a href="homepage.php" class="titlebox-href">GDH: Demonlist</a></li>
@@ -11,6 +12,7 @@
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "index"){echo 'active-nav-item';}?>" href="index.php">Main List</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "extended"){echo 'active-nav-item';}?>" href="extended.php">Extended List</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "legacy"){echo 'active-nav-item';}?>" href="legacy.php">Legacy List</a>
+            <a class="nav-buttons <?php if($_SESSION['active-nav'] == "Insane Demon List"){echo 'active-nav-item';}?>" href="InsaneDemonList.php">Insane Demon List</a>
             <a class="nav-buttons <?php if($_SESSION['active-nav'] == "leaderboard"){echo 'active-nav-item';}?>" href="leaderboard.php">Leaderboard</a>
     <a class="nav-buttons <?php if($_SESSION['active-nav'] == "platformerlist"){echo 'active-nav-item';}?>" href="https://gdhplatformers.hpsk.me/">Platformers</a>
 </div>
